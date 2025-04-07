20b52a2e2d25813bf7f53fb8e08954e21710b3d4
Franklin <franklinpereira98@hotmail.com>
2025-04-07 18:01:14 -0300

    update modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { QuizzComponent } from './components/quizz/quizz.component';
 import { HomeComponent } from './pages/home/home.component';
+import { QuizzComponent } from './components/quizz/quizz.component';
+
+// Importar FontAwesome
+//import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuizzComponent,
-    HomeComponent
+    HomeComponent,
+    QuizzComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    RouterModule,
+    AppRoutingModule,
+    //FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
